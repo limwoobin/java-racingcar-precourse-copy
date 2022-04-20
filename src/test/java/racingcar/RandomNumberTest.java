39d2dfa4587d759c47c8f07db1114ec2eecc7e32
limwoobin <drogba02@naver.com>
2022-04-20 18:51:29 +0900

    feat: 전진하는 조건을 제시하는 0,9 사이의 랜덤값을 제공하는 기능에 대해 테스트 코드를 작성하여 검증하고, 기능을 구현하라

---
+++ src/test/java/racingcar/RandomNumberTest.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import racingcar.domain.RandomNumber;
+
+public class RandomNumberTest {
+
+    @DisplayName("랜덤값을 추출하면 0~9 사이의 값이어야 한다")
+    @Test
+    void random_test() {
+        RandomNumber randomNumber = new RandomNumber();
+
+        assertThat(randomNumber.getRandomNumber())
+                .isGreaterThanOrEqualTo(0)
+                .isLessThanOrEqualTo(9);
+    }
+}
